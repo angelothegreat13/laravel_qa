0bb735dad6c72ea02ddf184d2f379d16190adac4
Angelo <angelothegreat13@gmail.com>
2018-12-09 21:18:46 +0800

    Display all questions

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('questions','QuestionsController');
