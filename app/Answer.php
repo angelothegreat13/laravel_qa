561061ca80ef741d1f4cf91c37f5580d89637efa
Angelo <angelothegreat13@gmail.com>
2019-01-20 23:58:53 +0800

    Update best_answer_id on questions table when answer removed using Eloquent Event

--- app/Answer.php
+++ app/Answer.php
@@ -37,8 +37,20 @@ class Answer extends Model
         });
 
         static::deleted(function($answer){
+            $question = $answer->question;
+            
             $answer->question->decrement('answers_count');
+            
+            if ($question->best_answer_id === $answer->id) {
+                $question->best_answer_id = NULL;
+                $question->save();
+            }
         });
+    }
+
+    public function getStatusAttribute()
+    {
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
     }
     
 
