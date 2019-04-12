a469028e8b245af64763e200f9b3e99483b8f6a2
Angelo <angelothegreat13@gmail.com>
2019-04-13 03:42:54 +0800

    Implement accept the answer as best answer

--- app/Answer.php
+++ app/Answer.php
@@ -41,10 +41,19 @@ class Answer extends Model
         });
     }
 
+    public function isBest()
+    {
+        return $this->id === $this->question->best_answer_id;
+    }
+
     public function getStatusAttribute()
     {
-        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
+        return $this->isBest() ? 'vote-accepted' : '';
     }
     
+    public function getIsBestAttribute()
+    {
+        return $this->isBest();
+    }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,5 @@ Route::resource('questions','QuestionsController')->except('show'); //removing s
 // Route::post('/questions/{question}/answers','AnswersController@store')->name('answers.store');
 Route::resource('questions.answers','AnswersController')->except(['index', 'create', 'show']);
 Route::get('/questions/{slug}','QuestionsController@show')->name('questions.show'); //custom route for show
+Route::post('/answers/{answer}/accept','AcceptAnswerController')->name('answers.accept'); //this means laravel will called the __invoker method automatically because there is no method name
 
